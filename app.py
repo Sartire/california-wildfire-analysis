f6759caafb875de2516f2ef95fcc3db85ed5b189
Antoine Edelman <aedelman9@gmail.com>
2021-04-17 10:46:46 -0400

    Added a graph with Average fire size by catalyst type

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ app = dash.Dash()
 
 fires = pd.read_csv('./data/fires_cleaned/final_fires_cleaned.csv')
 fires['STCT_FIPS'] = fires['STCT_FIPS'].apply(lambda x: '{0:0>5}'.format(x))
-fires= fires[fires['FIRE_YEAR'] >= 2003]
+fires = fires[fires['FIRE_YEAR'] >= 2003]
 years = fires['FIRE_YEAR'].unique()
-
 
 app.layout = html.Div(
     id="root",
@@ -22,14 +21,14 @@ app.layout = html.Div(
         html.Div(
             id="header",
             children=[
-                html.Img(id="logo", src=app.get_asset_url("Picture2.png"), style={'height':'15%', 'width':'15%'}),
+                html.Img(id="logo", src=app.get_asset_url("Picture2.png"), style={'height': '15%', 'width': '15%'}),
                 html.H4(children="Number of California Wildfires"),
                 html.P(
                     id="description",
                     children="Number of wildfires in California aggregated by county and year.",
-                    ),
-                ],
-            ),
+                ),
+            ],
+        ),
         html.Div(
             id="app-container",
             children=[
@@ -72,17 +71,21 @@ app.layout = html.Div(
                         dcc.Dropdown(
                             options=[
                                 {
-                                    "label": "Histogram of fire catalysts (single year)",
+                                    "label": "Histogram of fire catalysts count (single year)",
                                     "value": "show_fire_catalysts_single_year",
-                                    },
+                                },
                                 {
                                     "label": "Most destructive fires (single year)",
                                     "value": "show_largest_fires_table_single_year",
-                                    },
-                                ],
+                                },
+                                {
+                                    "label": "Histogram of fire catalysts average (single year)",
+                                    "value": "show_fire_catalysts_avg_single_year",
+                                }
+                            ],
                             value="show_fire_catalysts_single_year",
                             id="chart-dropdown",
-                            ),
+                        ),
                         dcc.Graph(
                             id="selected-data",
                             figure=dict(
@@ -92,49 +95,76 @@ app.layout = html.Div(
                                     plot_bgcolor="#F4F4F8",
                                     autofill=True,
                                     margin=dict(t=0, r=0, b=0, l=0),
-                                    ),
                                 ),
                             ),
-                        ],
-                    ),
-                ],
-            ),
-        ],
-    )
+                        ),
+                    ],
+                ),
+            ],
+        ),
+    ],
+)
+
 
 def getYearlyDataDict(years):
-  yearlyData = {}
-  for year in years:
-    filtered = fires[fires['FIRE_YEAR'] == year]
-    yearlyData[year] = filtered
-  return yearlyData
+    yearlyData = {}
+    for year in years:
+        filtered = fires[fires['FIRE_YEAR'] == year]
+        yearlyData[year] = filtered
+    return yearlyData
+
 
 yearlyData = getYearlyDataDict(years)
 
+
+# Not used
 def getFireCountsByYear(year):
     yearDF = yearlyData.get(year)
-    filtered_fips = yearDF['OBJECTID'].groupby(yearDF['STCT_FIPS']).count()
+    filtered_fips = yearDF['OBJECTID'].groupby(yearDF['STCT_FIPS']).count().sort_values()
     filtered_fips = filtered_fips.to_frame()
     filtered_fips.reset_index(inplace=True)
-    filtered_fips = filtered_fips.rename(columns={'OBJECTID': 'fire_count', 'STCT_FIPS':'fips'})
+    filtered_fips = filtered_fips.rename(columns={'OBJECTID': 'fire_count', 'STCT_FIPS': 'fips'})
     return filtered_fips
 
+
+# For "Histogram of fire catalysts count (single year)" graph aka "show_fire_catalysts_single_year"
 def getFireCatalystsByYear(year):
     yearDF = yearlyData.get(year)
-    catalysts = yearDF['OBJECTID'].groupby(yearDF['STAT_CAUSE_DESCR']).count()
+    catalysts = yearDF['OBJECTID'].groupby(yearDF['STAT_CAUSE_DESCR']).count().sort_values()
     catalysts = catalysts.to_frame()
     catalysts.reset_index(inplace=True)
-    catalysts = catalysts.rename(columns={'OBJECTID': 'fire_count', 'STAT_CAUSE_DESCR':'catalyst'})
-    return catalysts 
+    catalysts = catalysts.rename(columns={'OBJECTID': 'fire_count', 'STAT_CAUSE_DESCR': 'catalyst'})
+    return catalysts
 
+# For "Most destructive fires (single year)", aka "show_largest_fires_table_single_year"
 def getMostAcresBurntFipsByYear(year):
     yearDF = yearlyData.get(year)
-    acresBurnt = yearDF['FIRE_SIZE'].groupby(yearDF['STCT_FIPS']).sum()
+    acresBurnt = yearDF['FIRE_SIZE'].groupby(yearDF['STCT_FIPS']).sum().sort_values()
     acresBurnt = acresBurnt.to_frame()
     acresBurnt.reset_index(inplace=True)
-    acresBurnt = acresBurnt.rename(columns={'FIRE_SIZE': 'total_acres_burnt', 'STCT_FIPS':'fips'})
+    acresBurnt = acresBurnt.rename(columns={'FIRE_SIZE': 'total_acres_burnt', 'STCT_FIPS': 'fips'})
     acresBurnt = acresBurnt.sort_values(by='total_acres_burnt', ascending=False)[:10]
     return acresBurnt
+
+# For "Histogram of fire catalysts average (single year)" graph aka "show_fire_catalysts_avg_single_year"
+def getAvgFireCatalystsByYear(year):
+    yearDF = yearlyData.get(year)
+    catalysts = yearDF.groupby(yearDF['STAT_CAUSE_DESCR'])['FIRE_SIZE'].mean().sort_values()
+    catalysts = catalysts.to_frame()
+    catalysts.reset_index(inplace=True)
+    catalysts = catalysts.rename(columns={'FIRE_SIZE': 'fire_avg_size', 'STAT_CAUSE_DESCR': 'catalyst'})
+    return catalysts
+
+# # Will not work until the FOD_ID is in the final_fires_cleaned data set
+# def getMostAcresBurntCountyByYear(year):
+#     yearDF = yearlyData.get(year)
+#     acresBurnt = yearDF['FIRE_SIZE'].groupby(yearDF['FOD_ID']).sum().sort_values()
+#     acresBurnt = acresBurnt.to_frame()
+#     acresBurnt.reset_index(inplace=True)
+#     acresBurnt = acresBurnt.rename(columns={'FIRE_SIZE': 'total_acres_burnt', 'FOD_ID': 'County'})
+#     acresBurnt = acresBurnt.sort_values(by='total_acres_burnt', ascending=False)[:10]
+#     return acresBurnt
+
 
 def getCaliGeoJson():
     with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
@@ -146,7 +176,9 @@ def getCaliGeoJson():
     caliDict = {"features": cali, 'type': 'FeatureCollection'}
     return caliDict
 
+
 cali = getCaliGeoJson()
+
 
 @app.callback(
     Output('cali-wildfires', 'figure'),
@@ -176,15 +208,16 @@ def update_figure(selected_year):
                                         marker_line_width=0)
                     )
     fig.update_layout(mapbox_style="carto-positron",
-                      mapbox_zoom=5.2, mapbox_center = {"lat": 37.502236, "lon": -120.962930})
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+                      mapbox_zoom=5.2, mapbox_center={"lat": 37.502236, "lon": -120.962930})
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return fig
+
 
 @app.callback(
     Output("selected-data", "figure"),
     [
-     Input('year-slider', 'value'),
-     Input("chart-dropdown", "value"),
+        Input('year-slider', 'value'),
+        Input("chart-dropdown", "value"),
     ],
 )
 def update_chart(selected_year, chart_dropdown):
@@ -194,23 +227,11 @@ def update_chart(selected_year, chart_dropdown):
     elif chart_dropdown == "show_largest_fires_table_single_year":
         acres_burnt_by_year = getMostAcresBurntFipsByYear(selected_year)
         fig = px.bar(acres_burnt_by_year, x='fips', y='total_acres_burnt', color="total_acres_burnt")
+    elif chart_dropdown == "show_fire_catalysts_avg_single_year":
+        catalysts_by_year_avg = getAvgFireCatalystsByYear(selected_year)
+        fig = px.bar(catalysts_by_year_avg, x='catalyst', y='fire_avg_size', color="fire_avg_size")
     return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
