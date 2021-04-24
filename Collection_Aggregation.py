5153380e07774d5f93e8a95d9511f1bd1080da4e
Sartire <alexvdeluca@gmail.com>
2021-04-24 17:13:28 -0400

    Update Collection_Aggregation.py
    comment out second dist :(

--- Collection_Aggregation.py
+++ Collection_Aggregation.py
@@ -215,7 +215,7 @@ class ChartCreator(FireAggregations):
             fig_data = fig["data"]
             fig_data[0]["marker"]["color"] = "#fd6e6e"
             fig_data[0]["marker"]["opacity"] = .8
-            fig_data[1]["marker"]["opacity"] = .8
+            #fig_data[1]["marker"]["opacity"] = .8
         fig_layout["paper_bgcolor"] = "#242424"
         fig_layout["plot_bgcolor"] = "#242424"
         fig_layout["font"]["color"] = "#fd6e6e"
@@ -317,8 +317,8 @@ class ChartCreator(FireAggregations):
            
             yearsize = self.yearlyData.get(self.year)['FIRE_SIZE']
 
-            fig = ff.create_distplot([np.log(self.allsize),np.log(yearsize)],["2003-2015", 'In ' + str(self.year)], bin_size=10, show_hist = False, show_rug = False)
-            #fig = ff.create_distplot([ yearsize],['In' + str(self.year)], )
+            #fig = ff.create_distplot([np.log(self.allsize),np.log(yearsize)],["2003-2015", 'In ' + str(self.year)], bin_size=10, show_hist = False, show_rug = False)
+            fig = ff.create_distplot([ np.log(yearsize)],['In ' + str(self.year)],bin_size=1, show_hist = False, show_rug = False)
             fig.update_layout(title_text='Distribution of Fire Size')
             self.ChartStyling(fig, t = 'H', xLabel = 'ln(Fire Size)')
 
